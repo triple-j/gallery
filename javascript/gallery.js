f398b4e7e79920a322712501b345a75bffaef685
Jeremie J. Jarosh <jeremie@jarosh.org>
2014-02-08 18:32:19 -0600

    Move JavaScript into seperate file (cont.)

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -6,24 +6,52 @@
 
 		this.file = {
 			"VIEWER"       : options.VIEWER,
-			"AJAX_LISTING" : options.AJAX_LISTING
+			"AJAX_LISTING" : options.AJAX_LISTING,
+			"AJAX_FEATURE" : options.AJAX_FEATURE
 		};
 
 		this.current_page = options.current_page || 1;
 		this.default_page = this.current_page;
 		this.total_pages  = options.total_pages;
 
+		this.key = {
+			"LEFT_ARROW"  : 37,
+			"UP_ARROW"    : 38,
+			"RIGHT_ARROW" : 39,
+			"DOWN_ARROW"  : 40
+		};
+		this.nav_visible  = options.nav_visible || false;
+		this.current_img  = options.current_img;
+		this.default_img  = this.current_img;
+		this.imgs_per_nav = options.imgs_per_nav || 6;
 	};
 
-	Gallery.prototype.init = function(){
+	Gallery.prototype.init = function( type ){
 		var gallery = this;
+		type = type || "listing";
 
-		$(window).on('hashchange', function(evt){
-			gallery.change_page(evt, gallery);
-		});
-		this.change_page(null, gallery);
+		if ( type == "listing" ) {
 
-		$('#gallery').on('click', '.img-thumb-box a', function(evt){
+			$(window).on('hashchange', function(evt){
+				gallery.change_page(evt, gallery);
+			});
+			this.change_page(null, gallery);
+
+		} else if ( type == "viewer" ) {
+
+			$(window).on('hashchange', function(evt){
+				gallery.change_img(evt, gallery);
+			});
+			this.change_img(null, gallery);
+			this.display_nav();
+
+			$(document).on('keypress', function(evt){
+				gallery.key_pressed(evt, gallery);
+			});
+
+		}
+
+		$('#gallery, #thumb-nav').on('click', '.img-thumb-box a', function(evt){
 			gallery.open_image(evt, gallery);
 		});
 	};
@@ -56,9 +84,7 @@
 
 		console.log( {page:page, current:gallery.current_page} );
 
-		var AJAX_LISTING = gallery.file.AJAX_LISTING;
-
-		$('#gallery').load(AJAX_LISTING, {page:gallery.current_page});
+		$('#gallery').load(gallery.file.AJAX_LISTING, {page:gallery.current_page});
 	};
 
 
@@ -70,5 +96,96 @@
 		location.href = gallery.file.VIEWER + "#img:" + img_link;
 		evt.preventDefault();
 	};
+
+
+	///// viewer /////
+
+	Gallery.prototype.key_pressed = function(evt, gallery){
+		var pressed = (evt.keyCode || evt.charCode);
+
+		console.log( {pressed:pressed} );
+
+		switch ( pressed ) {
+			case gallery.key['LEFT_ARROW']:
+				console.log( "View Previous Image" );
+				gallery.prev_img();
+				break;
+			case gallery.key['RIGHT_ARROW']:
+				console.log( "View Next Image" );
+				gallery.next_img();
+				break;
+			case gallery.key['UP_ARROW']:
+				console.log( "Open Navigation" );
+				gallery.open_nav(gallery);
+				break;
+			case gallery.key['DOWN_ARROW']:
+				console.log( "Close Navigation" );
+				gallery.close_nav(gallery);
+				break;
+		}
+	};
+
+
+	Gallery.prototype.change_img = function(evt, gallery) {
+		var image = location.hash.replace(/^#img:/,"");
+
+		console.log( {image:image, current:gallery.current_img} );
+
+		switch ( image ) {
+			case "prev":
+				gallery.prev_img();
+				break;
+			case "next":
+				gallery.next_img();
+				break;
+			case "":
+				gallery.current_img = gallery.default_img;
+				break;
+			default:
+				gallery.current_img = image;
+		}
+
+		$('#viewer').load(gallery.file.AJAX_FEATURE, {img:gallery.current_img,preload:0}, function(){
+			if ( gallery.nav_visible ) {
+				gallery.open_nav(gallery);
+			}
+		});
+	};
+
+	Gallery.prototype.prev_img = function() {
+		var prev_href = $('#viewer .nav-links a.prev').attr('href');
+		location.hash = "#img:" + prev_href;
+	};
+
+	Gallery.prototype.next_img = function() {
+		var next_href = $('#viewer .nav-links a.next').attr('href');
+		location.hash = "#img:" + next_href;
+	};
+
+	Gallery.prototype.display_nav = function( visible ) {
+		if ( visible === undefined ) {
+			visible = this.nav_visible;
+		} else {
+			this.nav_visible = visible;
+		}
+
+		if ( visible ) {
+			$('#thumb-nav').show();
+		} else {
+			$('#thumb-nav').hide();
+		}
+	};
+
+	Gallery.prototype.open_nav = function(gallery) {
+		$('#thumb-nav').load(gallery.file.AJAX_LISTING, {numof:gallery.imgs_per_nav,rel:gallery.current_img}, function(){
+			console.log("Thumb Navigation Updated");
+			gallery.display_nav( true );
+		});
+	};
+
+	Gallery.prototype.close_nav = function(gallery) {
+		gallery.display_nav( false );
+	};
+
 
 })( jQuery, window );
--- viewer.php
+++ viewer.php
@@ -12,119 +12,22 @@ $first_img = $data['images'][0];
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
 		<link href="css/main.css" rel="stylesheet" type="text/css" media="screen">
-		<!--script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script-->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		<script src="javascript/gallery.js"></script>
 		<script>
-			var current_img = "<?=$first_img;?>";
-			var key = {
-				"LEFT_ARROW"  : 37,
-				"UP_ARROW"    : 38,
-				"RIGHT_ARROW" : 39,
-				"DOWN_ARROW"  : 40
-			};
-			var nav_visible  = false;
 
-			function change_img(e) {
-				var image = location.hash.replace(/^#img:/,"");
-
-				console.log( {image:image, current:window.current_img} );
-
-				/*if ( image != "" ) {
-					window.current_img = image;
-				}*/
-				switch ( image ) {
-					case "prev":
-						prev_img();
-						break;
-					case "next":
-						next_img();
-						break;
-					case "":
-						window.current_img = "<?=$first_img;?>";
-						break;
-					default:
-						window.current_img = image;
-				}
-
-				$('#viewer').load('feature.php', {img:window.current_img,preload:0}, function(){
-					if ( window.nav_visible ) {
-						open_nav();
-					}
-				});
-			}
-
-			function prev_img() {
-				var prev_href = $('#viewer .nav-links a.prev').attr('href');
-				location.hash = "#img:" + prev_href;
-			}
-
-			function next_img() {
-				var next_href = $('#viewer .nav-links a.next').attr('href');
-				location.hash = "#img:" + next_href;
-			}
-
-			function display_nav( visible ) {
-				if ( visible === undefined ) {
-					visible = window.nav_visible;
-				} else {
-					window.nav_visible = visible;
-				}
-
-				if ( visible ) {
-					$('#thumb-nav').show();
-				} else {
-					$('#thumb-nav').hide();
-				}
-			}
-
-			function open_nav() {
-				$('#thumb-nav').load('<?=FILE_AJAX_LISTING;?>', {numof:<?=IMAGES_PER_NAV;?>,rel:window.current_img}, function(){
-					console.log("Thumb Navigation Updated");
-					display_nav( true );
-				});
-			}
-
-			function close_nav() {
-				display_nav( false );
-			}
+			var gallery_viewer = new Gallery({
+				"VIEWER"       : "<?=FILE_VIEWER;?>",
+				"AJAX_LISTING" : "<?=FILE_AJAX_LISTING;?>",
+				"AJAX_FEATURE" : "<?=FILE_AJAX_FEATURE;?>",
+				"current_img"  : "<?=$first_img;?>",
+				"imgs_per_nav" : "<?=IMAGES_PER_NAV;?>"
+			});
 
 			$(document).ready(function(){
-				$(window).on('hashchange', change_img);
-				change_img(null);
-				display_nav();
-
-				$(document).on('keypress', function(evt){
-					var pressed = (evt.keyCode || evt.charCode);
-
-					console.log( {pressed:pressed} );
-
-					switch ( pressed ) {
-						case window.key['LEFT_ARROW']:
-							console.log( "View Previous Image" );
-							prev_img();
-							break;
-						case window.key['RIGHT_ARROW']:
-							console.log( "View Next Image" );
-							next_img();
-							break;
-						case window.key['UP_ARROW']:
-							console.log( "Open Navigation" );
-							open_nav();
-							break;
-						case window.key['DOWN_ARROW']:
-							console.log( "Close Navigation" );
-							close_nav();
-							break;
-					}
-				});
-
-				$('#thumb-nav').on('click', '.img-thumb-box a', function(evt){
-					var img_link = $(this).attr('href');
-					//console.log( {img_link:img_link, encode:encodeURIComponent(img_link)} );
-					location.href = "<?=FILE_VIEWER;?>#img:" + img_link;
-					evt.preventDefault();
-				});
+				gallery_viewer.init('viewer');
 			});
+
 		</script>
 	</head>
 
