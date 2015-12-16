4abdffda331fbd0640bc3895c63868906f277083
Jeremie J. Jarosh <jeremie@jarosh.org>
2015-12-16 09:35:44 -0600

    Remove old/unused files

--- config.json.php
+++
@@ -1,7 +0,0 @@
-<?php
-include( 'config.php' );
-
-$all_consts = get_defined_constants(true);
-
-header('Content-type: application/json; charset=utf-8');
-echo json_encode( $all_consts['user'], JSON_PRETTY_PRINT );
